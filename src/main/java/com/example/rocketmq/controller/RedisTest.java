a61f70e420c99a96f4d38d05148a06d094c5e5fa
注suguanx1n <sgux1nn77@gmail.com>
2024-05-27 01:07:36 +0800

    小改动

--- src/main/java/com/example/rocketmq/controller/RedisTest.java
+++ src/main/java/com/example/rocketmq/controller/RedisTest.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,7 +22,8 @@ import java.util.concurrent.TimeUnit;
 public class RedisTest {
 
 
-    @Autowired
+    //@Autowired
+    @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
 
